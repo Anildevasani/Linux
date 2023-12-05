68c07358225e6487163183da9feae4b2f8b4a6ba
Anildevasani <129273148+Anildevasani@users.noreply.github.com>
2023-12-05 14:48:16 +0530

    Delete filordir.sh

--- filordir.sh
+++
@@ -1,9 +0,0 @@
-Write a shell script that displays a list of all the files in the current directory.
-
-#!/bin/bash
-
-echo "List of files in the current directory: "
-
-for Documents in *; do
-    echo $Documents
-done
